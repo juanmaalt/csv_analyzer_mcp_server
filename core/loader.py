9493458d14825fee907ec9d60b5e066dd9a8ee11
Juan M Altamirano <juanmaalt@gmail.com>
2026-02-28 23:40:41 +0100

    minor changes

--- core/loader.py
+++ core/loader.py
@@ -2,16 +2,20 @@ import pandas as pd
 from io import StringIO
 
 def load_csv(csv_content: str, delimiter: str, remove_duplicates: bool, remove_non_valid_data: bool):
-    # Convert .csv into a Data frame 
+    # Convert .csv into a DataFrame
     buffer = StringIO(csv_content)
-    df = pd.read_csv(buffer, delimiter= delimiter)
+    df = pd.read_csv(buffer, delimiter=delimiter)
+
+    original_row_count = len(df)
 
     # Remove null values
-    if (remove_non_valid_data):
+    if remove_non_valid_data:
         df = df.dropna()
 
     # Remove duplicates
-    if (remove_duplicates):
-        df.drop_duplicates(inplace = True)
+    if remove_duplicates:
+        df.drop_duplicates(inplace=True)
 
-    return df
+    df = df.reset_index(drop=True)
+
+    return df, original_row_count
